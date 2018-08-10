d2f2c9512db9ad3f1a85a059a16696e2f5db9f58
Shivam Pokhriyal <itechshivam2@gmail.com>
2018-08-10 10:10:39 +0530

    week2

---
+++ week2/script/script.js
@@ -0,0 +1,14 @@
+
+
+var submit = document.getElementById("sub");
+
+var text = document.getElementById("text");
+
+function change() {
+    
+    document.getElementById("text").innerHTML="Thanks! We will get back to you SOON!";
+
+}
+
+
+// window.alert("s");
